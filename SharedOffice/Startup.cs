fe2c0574b5100578cf9eae6f9879550e14ac141d
Orlando Rodriguez <ORodriguez@emphasys-software.com>
2016-07-12 22:07:23 -0400

    Add project files.

---
+++ SharedOffice/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SharedOffice.Startup))]
+namespace SharedOffice
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
